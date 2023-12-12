e9751d7ae5d682863e47afb761a293c72d18b7db
ItsRandomNameMeh <d.enotow@yandex.ru>
2023-12-13 00:30:24 +0300

    logs v2.0

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QFileDialog, QPushButton, QVBoxLayout, QTextEdit, QScrollArea, \
     QGroupBox, QFormLayout
 
-import logicclass
+
 from logicclass import MinskCounterMachine
 
 
